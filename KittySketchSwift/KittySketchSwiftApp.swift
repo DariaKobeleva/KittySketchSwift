c6683cae807c30db2bc86aa0738c89963bda389f
KDaria <dkdva@mail.ru>
2024-05-07 11:39:26 +0200

    Initial Commit

---
+++ KittySketchSwift/KittySketchSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  KittySketchSwiftApp.swift
+//  KittySketchSwift
+//
+//  Created by Дарья Кобелева on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct KittySketchSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
